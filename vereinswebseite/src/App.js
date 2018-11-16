2b9d68492ed4d267f6e3b0ec5fe08f03dc44ae1a
Carina Wehr <wehr.carina@gmail.com>
2018-11-16 11:42:41 +0100

    NavBar and App.js

--- vereinswebseite/src/App.js
+++ vereinswebseite/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -17,3 +16,40 @@ class App extends Component {
 }
 
 export default App;
+
+// const styles = {
+//   root: {
+//     flexGrow: 1,
+//   },
+//   grow: {
+//     flexGrow: 1,
+//   },
+//   menuButton: {
+//     marginLeft: -12,
+//     marginRight: 20,
+//   },
+// };
+
+// function ButtonAppBar(props) {
+//   const { classes } = props;
+//   return (
+//     <div className={classes.root}>
+//       <AppBar position="static">
+//         <Toolbar>
+//           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+//           </IconButton>
+//           <Typography variant="h6" color="inherit" className={classes.grow}>
+//             News
+//           </Typography>
+//           <Button color="inherit">Login</Button>
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
+
+// ButtonAppBar.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// };
+
+// export default withStyles(styles)(ButtonAppBar);
