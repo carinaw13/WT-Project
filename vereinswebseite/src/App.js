d78c78b315d9b291f7b6cf0cf07a8aae2e280a3a
Carina Wehr <wehr.carina@gmail.com>
2018-11-19 20:32:00 +0100

    Startseite bearbeitet

--- vereinswebseite/src/App.js
+++ vereinswebseite/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
+import React, { Component } from 'react'
+import './App.css'
+import NavBar from './components/NavBar'
 
 
 
@@ -9,53 +9,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Hello World. Carina 
-          </p>
-          <p>
+          <p> Startseite SC Baierbrunn </p>
+          <p> Spass am Sport </p>
         <NavBar />
-          </p>
         </header>
-      </div>
+       </div>
     );
   }
 }
 
 export default App;
-
-// const styles = {
-//   root: {
-//     flexGrow: 1,
-//   },
-//   grow: {
-//     flexGrow: 1,
-//   },
-//   menuButton: {
-//     marginLeft: -12,
-//     marginRight: 20,
-//   },
-// };
-
-// function ButtonAppBar(props) {
-//   const { classes } = props;
-//   return (
-//     <div className={classes.root}>
-//       <AppBar position="static">
-//         <Toolbar>
-//           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-//           </IconButton>
-//           <Typography variant="h6" color="inherit" className={classes.grow}>
-//             News
-//           </Typography>
-//           <Button color="inherit">Login</Button>
-//         </Toolbar>
-//       </AppBar>
-//     </div>
-//   );
-// }
-
-// ButtonAppBar.propTypes = {
-//   classes: PropTypes.object.isRequired,
-// };
-
-// export default withStyles(styles)(ButtonAppBar);
