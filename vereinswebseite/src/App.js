2a7abcac2682f45e8d903ce0a42e616d46c82077
Carina Wehr <wehr.carina@gmail.com>
2018-11-26 19:26:29 +0100

    hinzufügen von Impressum

--- vereinswebseite/src/App.js
+++ vereinswebseite/src/App.js
@@ -5,21 +5,31 @@ import logo from './RZ_Logo_Baierbrunn.svg'
 
 
 
-
-
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <p><img src={logo} className='App-logo' alt='logo' />  </p>
+      <p><img style= {{float:"left"}}src={logo} className='App-logo' alt='logo' />  </p>
         <header className="App-header">
           <h1> <i> SportClub Baierbrunn e.V.</i> </h1>
           <h2 style={{paddingLeft: "8.000em"}}> <i> Spass am Sport </i> </h2>
         </header>
         <Startseite />
+      <footer 
+        style= {{
+        padding: "10px",
+        bottom: "0",
+        left: "0",
+        right: "0" }}
+        >
+          <div class="wrapper">
+       <a href="kontakt.htm">Kontakt</a><br />
+       <a href="impressum.htm">Impressum</a>
+				<p>© 2014 by selfHTML</p>
+			</div>
+        </footer>
        </div>
+       
     );
   }
 }
