f4e102aee8e0159dfd64dbb06b30ceefbd343de4
Helena Leitner <helena.leitner@stud.fh-rosenheim.de>
2018-12-11 18:32:37 +0100

    Abteilungsseiten

---
+++ vereinswebseite/src/pages/abtSki.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import './App.css'
+
+
+class AbtBasketball extends Component {
+    render() {
+        return (
+            <div class="abt-mainbody-content">
+                <div class="page-header">
+                    <h1>Skiabteilung</h1>
+                </div>
+                <div class="page-body">
+                    <h4>Abteilungsleiter</h4>
+                    <div>
+                        Tobias Rapp
+                    <br />
+                        Forstenrieder Weg 3a
+                    <br />
+                        82065 Baierbrunn
+                    <br />
+                        Telefon: 089-45345023
+                    <br />
+                        E-Mail:
+                    <span>
+                            <a href="mailto:tobiasrapp@t-online.de">tobiasxrapp@t-online.de</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AbtBasketball;
