2329a7b87741d10192b2b8e10322dc9139bd0cd3
Julia Gettikh <julia.gettikh@stud.fh-rosenheim.de>
2018-12-12 15:21:12 +0100

    Gemeinde, Kontakt

---
+++ vereinswebseite/src/pages/ueber-uns/kontakt.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './App.css'
+
+
+class Kontakt extends Component {
+  render() {
+    return (
+      
+      <div class="ueberUns-mainbody-content">
+      <div class="page-header">
+          <h1>Kontakt</h1> 
+          <adress>
+                <h3>1. Vorsitzender Gerold Wehr</h3>
+                <p><strong>Tel:</strong> 089-79360736</p>
+                <a href="mailto:info@sc-baierbrunn.de">info@sc-baierbrunn.de</a><br></br>
+  
+              </adress>
+      </div> 
+      </div>
+
+    );
+  }
+}
+
+export default Kontakt;
