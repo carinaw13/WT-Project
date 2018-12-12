180855066d6b9a0c81f6fc9da5d2329fab010e8f
Carina Wehr <wehr.carina@gmail.com>
2018-12-12 10:59:44 +0100

    add new Startseite.js

---
+++ vereinswebseite/src/pages/startseite.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import '../App.css'
+
+
+
+class Startseite extends Component {
+    render() {
+        return (
+            <div class="abt-mainbody-content">
+                <div class="page-header">
+                    <h1>YoLO</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Startseite;
