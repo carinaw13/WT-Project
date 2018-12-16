4f323b0c9ba828fa7d3bf444c27d528ecaaf241f
Helena Leitner <helena.leitner@stud.fh-rosenheim.de>
2018-12-16 16:08:58 +0100

    express tests

--- vereinswebseite/src/app2.js
+++ vereinswebseite/src/app2.js
@@ -1,29 +1,65 @@
-const express = require('express')  // load the node express module
-const bp = require('body-parser')
-const mysql = require('mysql2/promise')  // wrap everything in promises
-const app = express()  // create a new express app
+const express = require("express"); // load the node express module
+const bp = require("body-parser");
+const mysql = require("mysql2/promise");
+const app = express(); // create a new express app
 
-
+/*
 mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: 'Gabel35!',
 	database: 'vereinswebseite_db'
-})
+})*/
+
+let db; // will be set below!
+mysql
+  .createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Gabel35!",
+    database: "vereinswebseite_db"
+  })
+  .then(connection => {
+    db = connection; // remember the db-handle!
+    return db.query("SELECT * FROM members");
+  })
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => console.log(err)); // log errors to console
+/*
+app.get("/members", function(req, res) {
+  connection.connect();
+  connection.query("SELECT * FROM members", function(error, results, fields) {
+    if (error) throw error;
+	res.send(results);
+	console.log(results);
+  });
+  connection.end();
+});*/
+
+app.get("/", (req, res, next) => {
+  console.log(req.headers);
+  console.log(req.params);
+  console.log(req.body);
+  res.status(200).send("Hello");
+});
+
+app.get("/members", (req, res, next) => {
+  res.send("hello world");
+});
 
 
 // let's treat incoming request bodies as text/plain
-app.use(bp.text())
+app.use(bp.text());
 
 // this will catch any incoming request...
 app.use((req, res) => {
-	console.log([req.method, req.hostname, req.ip, req.url, req.headers])
-	console.log(req.query)
-	console.log(req.body)
-	res.status(200).end()  // sends 200 OK, no body, closes connection
-})
-
+  console.log([req.method, req.hostname, req.ip, req.url, req.headers]);
+  console.log(req.query);
+  console.log(req.body);
+  res.status(200).end(); // sends 200 OK, no body, closes connection
+});
 
 // start the webserver, listen on port 3000
-app.listen(3000, 
-	() => console.log('Example app.js listening on port 3000!'))
+app.listen(3000, () => console.log("Server ready on port 3000!"));
