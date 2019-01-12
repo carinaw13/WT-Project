0534e11da218ddb5a940fc49e882f4cac2ca5a19
Helena Leitner <helena.leitner@stud.fh-rosenheim.de>
2019-01-12 10:44:01 +0100

    mitgliedsantrag

--- vereinswebseite/src/pages/ueberUns/mitgliedsantrag.js
+++ vereinswebseite/src/pages/ueberUns/mitgliedsantrag.js
@@ -1,9 +1,133 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
-class Mitgliedsantrag extends Component {
+import ReactDOM from "react-dom";
+import Input from "@material-ui/core/Input";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import FilledInput from "@material-ui/core/FilledInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  dense: {
+    marginTop: 16
+  },
+  menu: {
+    width: 200
+  },
+  root: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120
+  }
+});
+
+class OutlinedTextFields extends React.Component {
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+
+  state = {
+    gender: ""
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
-    return <h1>Hallo</h1>;
+    const { classes } = this.props;
+
+    return (
+      <div className="form-div">
+        <h1>SportClub Baierbrunn e.V.</h1>
+        <h2>Aufnahmeantrag</h2>
+        <br />
+
+        <p className="left-p">
+          1. Ich beantrage hiermit meine Aufnahme als Mitglied im SC Baierbrunn
+          e.V.
+        </p>
+        <form className={classes.root} autoComplete="off">
+          <FormControl className={classes.formControl}>
+            <InputLabel>Geschlecht</InputLabel>
+            <Select
+              value={this.state.gender}
+              onChange={this.handleChange}
+              inputProps={{
+                name: "gender"
+              }}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={"männlich"}>männlich</MenuItem>
+              <MenuItem value={"weiblich"}>weiblich</MenuItem>
+              <MenuItem value={"divers"}>divers</MenuItem>
+            </Select>
+          </FormControl>
+        </form>
+
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="outlined-required"
+            label="Titel"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Vorname"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Name"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            id="outlined-email-input"
+            label="Email"
+            className={classes.textField}
+            type="email"
+            name="email"
+            autoComplete="email"
+            margin="normal"
+            variant="outlined"
+          />
+        </form>
+      </div>
+    );
   }
 }
 
-export default Mitgliedsantrag;
+OutlinedTextFields.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(OutlinedTextFields);
